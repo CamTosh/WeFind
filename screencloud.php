559cbd7422b1050dfb9405524f78a07090e8c49b
Tosh Camille <camilleminecraft@gmail.com>
2015-08-13 09:43:50 +0200

    gngngnngng
    il est tard

--- screencloud.php
+++ screencloud.php
@@ -33,7 +33,7 @@ window.onbeforeunload = function(){
 <?php 
 
 $tableau = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
-    for ($i=0; $i < 100; $i++) {
+	for ($i=0; $i < 100; $i++) {
         $olol = $tableau[rand(0,(count($tableau))-1)].$tableau[rand(0,(count($tableau))-1)].$tableau[rand(0,(count($tableau))-1)].$tableau[rand(0,(count($tableau))-1)];
 
  $toast = 'http://screencloud.net/v/'.$olol.'';
